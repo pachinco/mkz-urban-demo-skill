ac884f80d4397f36fea4b65efce9b6eba2d2e923
Holger Schanz <holger.schanz@me.com>
2022-09-30 16:56:07 -0700

    add homescreen

--- __init__.py
+++ __init__.py
@@ -1,11 +1,18 @@
 from mycroft import MycroftSkill, intent_file_handler
 from pathlib import Path
 from mycroft.util import play_wav
+from mycroft.skills import resting_screen_handler
 
 class MkzUrbanDemo(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.sound_file_path = Path(__file__).parent.joinpath("sounds", "mkz-welcome-chime2.wav")
+
+    @resting_screen_handler("Personalised homescreen")
+    def handle_homescreen(self, message):
+        mkzdemo_img = Path(__file__).parent.joinpath("images", "mkz-homescreen.png")
+        background_img = self.settings.get("background_img", mkzdemo_img)
+        self.gui.show_image(background_img)
 
     @intent_file_handler('demo.urban.mkz.intent')
     def handle_demo_urban_mkz(self, message):
