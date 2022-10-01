f92b43b541700ee00442c115d1662ed2a61d844f
Holger Schanz <holger.schanz@me.com>
2022-09-30 17:03:34 -0700

    add homescreen

--- __init__.py
+++ __init__.py
@@ -7,18 +7,19 @@ class MkzUrbanDemo(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.sound_file_path = Path(__file__).parent.joinpath("sounds", "mkz-welcome-chime2.wav")
+        self.mkzdemo_img = Path(__file__).parent.joinpath("images", "mkz-homescreen.png")
 
     @resting_screen_handler("Personalised homescreen")
     def handle_homescreen(self, message):
-        mkzdemo_img = Path(__file__).parent.joinpath("images", "mkz-homescreen.png")
-        background_img = self.settings.get("background_img", mkzdemo_img)
+        background_img = self.settings.get("background_img", self.mkzdemo_img)
         self.gui.show_image(background_img)
 
     @intent_file_handler('demo.urban.mkz.intent')
     def handle_demo_urban_mkz(self, message):
+        background_img = self.settings.get("background_img", self.mkzdemo_img)
+        self.gui.show_image(background_img)
         play_proc = play_wav(str(self.sound_file_path))
         self.speak_dialog('demo.urban.mkz')
-
 
 def create_skill():
     return MkzUrbanDemo()
