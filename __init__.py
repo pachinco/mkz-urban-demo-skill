bba1d55c8c08ddb55b5ca2c9c9e0c86617ef2a91
Holger Schanz <holger.schanz@me.com>
2022-09-30 18:17:03 -0700

    homescreen fix

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, resting_screen_handler
 from pathlib import Path
 from mycroft.util import play_wav
-from mycroft.skills import resting_screen_handler
+#from mycroft.skills import resting_screen_handler
 
 class MkzUrbanDemo(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.sound_file_path = Path(__file__).parent.joinpath("sounds", "mkz-welcome-chime2.wav")
         self.mkzdemo_img = Path(__file__).parent.joinpath("images", "mkz_homescreen.png")
+        self.settings.set("wallpaper_url", str(self.sound_file_path))
 
-    @resting_screen_handler("Personalised homescreen")
+    @resting_screen_handler("MKZ homescreen")
     def handle_homescreen(self, message):
 #        background_img = self.settings.get("background_img", self.mkzdemo_img)
 #        self.gui.show_image(self,str(self.mkzdemo_img),fill=Stretch,override_idle=True)
