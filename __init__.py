029296c86eaaec9dcb73e5911cc67630447be2f5
Holger Schanz <holger.schanz@me.com>
2022-09-30 19:34:49 -0700

    homescreen fix

--- __init__.py
+++ __init__.py
@@ -8,20 +8,19 @@ class MkzUrbanDemo(MycroftSkill):
         MycroftSkill.__init__(self)
         self.sound_file_path = Path(__file__).parent.joinpath("sounds", "mkz-welcome-chime2.wav")
         self.mkzdemo_img = Path(__file__).parent.joinpath("images", "mkz_homescreen.png")
-#        self.settings["wallpaper_url"]=str(self.mkzdemo_img)
+        self.settings['wallpaper_file']="custom-wallpaper.jpg"
+        self.settings['wallpaper_url']=str(self.mkzdemo_img)
 
-    @resting_screen_handler('MKZ homescreen')
-    def handle_homescreen(self, message):
+#    @resting_screen_handler('MKZ homescreen')
+#    def handle_homescreen(self, message):
 #        background_img = self.settings.get("background_img", self.mkzdemo_img)
-#        background_img = self.settings.get("background_img", self.mkzdemo_img)
-#        self.gui.show_image(self,str(self.mkzdemo_img),fill=Stretch,override_idle=True)
 #        self.gui.show_image(str(self.mkzdemo_img))
-        self.gui.show_image(str(self.sound_file_path))
+#        self.gui.show_image(str(self.sound_file_path))
 
     @intent_file_handler('demo.urban.mkz.intent')
     def handle_demo_urban_mkz(self, message):
 #        self.gui.show_image(self,str(self.mkzdemo_img),fill=Stretch,override_idle=True)
-        self.gui.show_image(str(self.mkzdemo_img))
+#        self.gui.show_image(str(self.mkzdemo_img))
         play_proc = play_wav(str(self.sound_file_path))
         self.speak_dialog('demo.urban.mkz')
 
