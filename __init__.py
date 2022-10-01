7d989db986a0d5bfd9219ca2c642ef8e6213aff0
Holger Schanz <holger.schanz@me.com>
2022-09-30 17:06:04 -0700

    fixed skill error

--- __init__.py
+++ __init__.py
@@ -16,8 +16,8 @@ class MkzUrbanDemo(MycroftSkill):
 
     @intent_file_handler('demo.urban.mkz.intent')
     def handle_demo_urban_mkz(self, message):
-        background_img = self.settings.get("background_img", self.mkzdemo_img)
-        self.gui.show_image(background_img)
+#        background_img = self.settings.get("background_img", self.mkzdemo_img)
+        self.gui.show_image(self.mkzdemo_img)
         play_proc = play_wav(str(self.sound_file_path))
         self.speak_dialog('demo.urban.mkz')
 
