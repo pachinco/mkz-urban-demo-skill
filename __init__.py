4efcbbfa77b1c508e9ee85c51013b84c4dd615a3
Holger Schanz <holger.schanz@me.com>
2022-09-30 19:28:05 -0700

    homescreen fix

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ class MkzUrbanDemo(MycroftSkill):
         self.mkzdemo_img = Path(__file__).parent.joinpath("images", "mkz_homescreen.png")
 #        self.settings["wallpaper_url"]=str(self.mkzdemo_img)
 
-    @resting_screen_handler("MKZ homescreen")
+    @resting_screen_handler('MKZ homescreen')
     def handle_homescreen(self, message):
 #        background_img = self.settings.get("background_img", self.mkzdemo_img)
 #        background_img = self.settings.get("background_img", self.mkzdemo_img)
@@ -21,7 +21,7 @@ class MkzUrbanDemo(MycroftSkill):
     @intent_file_handler('demo.urban.mkz.intent')
     def handle_demo_urban_mkz(self, message):
 #        self.gui.show_image(self,str(self.mkzdemo_img),fill=Stretch,override_idle=True)
-#        self.gui.show_image(str(self.mkzdemo_img))
+        self.gui.show_image(str(self.mkzdemo_img))
         play_proc = play_wav(str(self.sound_file_path))
         self.speak_dialog('demo.urban.mkz')
 
