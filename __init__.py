acfa35f831eaefc7d4f8f775eb7280068c7c99ee
Holger Schanz <holger.schanz@me.com>
2022-10-07 09:03:23 -0700

    add gui

--- __init__.py
+++ __init__.py
@@ -13,49 +13,32 @@ class MkzUrbanDemo(MycroftSkill):
         #self.mkz_ui = Path(__file__).parent.joinpath("ui", "mkz.qml")
         #self.mkz_9grid_ui = Path(__file__).parent.joinpath("ui", "mkz-9grid-buttons.qml")
         self.mkz_list_ui = Path(__file__).parent.joinpath("ui", "mkz-list-buttons.qml")
-        #self.actionsObject = {}
-        #self.actionsList = []
-        #self.actionsObject['actions'] = self.actionsList
-        #self.gui['actionsList'] = self.actionsObject
         self.gui['actionsList'] = []
 
-   #@resting_screen_handler('MKZ homescreen')
-   #def handle_homescreen(self, message):
-        #self.gui.clear()
-        #self.enclosure.display_manager.remove_active()
-        #background_img = self.settings.get("background_img", self.mkzdemo_img)
-        #self.gui.show_image(str(self.mkzdemo_img))
+   @resting_screen_handler('MKZ homescreen')
+   def handle_homescreen(self, message):
+        self.gui.clear()
+        self.enclosure.display_manager.remove_active()
+        background_img = self.settings.get("background_img", self.mkzdemo_img)
+        self.gui.show_image(str(self.mkzdemo_img), override_idle=True)
 
     @intent_file_handler('demo.urban.mkz.intent')
     def handle_demo_urban_mkz(self, message):
         self.gui.clear()
         self.enclosure.display_manager.remove_active()
         play_proc = play_wav(str(self.sound_file_path))
-        #self.actionsObject = {}
-        #self.actionsList = [{"text": "Activate", "image": "../images/Power-button.png"},
-                            #{"text": "Drive", "image": "../images/Start-button.png"},
-                            #{"text": "Setting", "image": "../images/Settings-symbol.png"}]
-        #self.actionsObject['actions'] = self.actionsList
-        #self.actionsList.append({"text": "Activate", "image": "../images/Power-button.png"})
-        #self.actionsList.append({"text": "Drive", "image": "../images/Start-button.png"})
-        #self.actionsList.append({"text": "Proceed", "image": "../images/Forward-button.png"})
-        #self.actionsList.append({"text": "Setting", "image": "../images/Settings-symbol.png"})
-        self.gui['actionsList'] = []
+        #self.gui['actionsList'] = []
         self.gui['background'] = str(self.mkzdemo_img)
-        self.gui.show_page(str(self.mkz_list_ui), override_idle=True)
+        #self.gui.show_page(str(self.mkz_list_ui), override_idle=True)
         self.speak_dialog('demo.urban.mkz', wait=True)
         self.schedule_event(self._ask_what_to_do, 5)
 
     def _ask_what_to_do(self):
         self.speak('What would you like to do?', expect_response=True, wait=True)
-        #self.actionsList.append({"text": "Activate", "image": "../images/Power-button.png"})
-        #self.actionsList.append({"text": "Drive", "image": "../images/Start-button.png"})
-        #self.actionsList.append({"text": "Proceed", "image": "../images/Forward-button.png"})
-        #self.actionsList.append({"text": "Setting", "image": "../images/Settings-symbol.png"})
         self.gui['actionsList'] = [{"text": "Activate", "image": "../images/Power-button.png"},
-                                        {"text": "Drive", "image": "../images/Start-button.png"},
-                                        {"text": "Setting", "image": "../images/Settings-symbol.png"}]
-        #self.gui['actionsList'] = self.actionsList
+                                    {"text": "Drive", "image": "../images/Start-button.png"},
+                                    {"text": "Setting", "image": "../images/Settings-symbol.png"}]
+        self.gui.show_page(str(self.mkz_list_ui), override_idle=True)
         
 def create_skill():
     return MkzUrbanDemo()
