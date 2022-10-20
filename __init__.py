85721dbbf660641b9b899f31bd05cfc00f3a4692
Holger Schanz <holger.schanz@me.com>
2022-10-20 16:44:45 -0700

    qml change

--- __init__.py
+++ __init__.py
@@ -36,7 +36,7 @@ class MkzUrbanDemo(MycroftSkill):
         self.enclosure.display_manager.remove_active()
         play_proc = play_wav(str(self.sound_file_path))
         self.gui['actionsList'] = []
-        self.gui.show_image("./images/mkz_background_center_day.png"), override_idle=True, override_animations=True)
+        self.gui.show_image("./images/mkz_background_center_day.png", override_idle=True, override_animations=True)
         #self.gui.show_page(str(self.mkz_list_ui), override_idle=True)
         self.speak_dialog('demo.urban.mkz', wait=True)
         self.schedule_event(self._ask_what_to_do, 5)
