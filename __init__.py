bdfaae7ce84855c40aaf9a9bc1087f3b6ab2f226
Holger Schanz <holger.schanz@me.com>
2022-10-20 11:40:08 -0700

    qml change

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ class MkzUrbanDemo(MycroftSkill):
         self.gui.clear()
         #self.enclosure.display_manager.remove_active()
         self.log.info('Activating MKZ homescreen')
-        self.gui.show_image(str(self.mkzdemo_img), fill=Stretch, override_idle=True)
+        self.gui.show_image(str(self.mkzdemo_img), fill=Stretch, override_idle=True, override_animations=True)
 
     @intent_file_handler('demo.urban.mkz.intent')
     def handle_demo_urban_mkz(self, message):
@@ -34,7 +34,7 @@ class MkzUrbanDemo(MycroftSkill):
         self.enclosure.display_manager.remove_active()
         play_proc = play_wav(str(self.sound_file_path))
         self.gui['actionsList'] = []
-        self.gui.show_image(str(self.mkzdemo_img))
+        self.gui.show_image(str(self.mkzdemo_img), fill=Stretch, override_idle=True, override_animations=True)
         #self.gui.show_page(str(self.mkz_list_ui), override_idle=True)
         self.speak_dialog('demo.urban.mkz', wait=True)
         self.schedule_event(self._ask_what_to_do, 5)
